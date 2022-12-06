9634889433ccb45348b4997efc9cc65d7c254b64
Rajsbhuva <119853250+Rajsbhuva@users.noreply.github.com>
2022-12-06 13:05:11 +0530

    Add files via upload

---
+++ 14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main int()
+{
+	int a;
+	prinf("enter tha no");
+	scanf(%d,%a);
+	if (a=0)
+	{
+		prinf(number is positive)
+    }
+    else
+    {
+    	printf(number is negetiv)
+    	
+	}
+}
+
