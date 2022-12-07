1fc6c7592934e991f734afc3811711ad825d3588
Rajsbhuva <119853250+Rajsbhuva@users.noreply.github.com>
2022-12-07 23:08:23 +0530

    Add files via upload

--- 23_nov_raj_/assignment/raj.c
+++ 23_nov_raj_/assignment/raj.c
@@ -1,8 +1,6 @@
-#include<stdio.h>
-void main()
-{
-    //printf()
-    printf("hello world");
-    
+#iniclude <stdio.h>
+void main{
+	int a;
+	for(a=0,a<=5120,a++)
+	printf("%d\t%d\%dt\n",a,a*a,a*a*a);
 }
-
